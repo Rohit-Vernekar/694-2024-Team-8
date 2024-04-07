ed4accd225eda3e0c7a84fea1ca997dd54641a17
Harshith Oberoi <oberoiharshith@gmail.com>
2024-04-07 16:17:06 -0400

    tweetsprocessing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.cache import Cache
 import time
+from src.tweet_data_processor import TweetDataProcessor
 
 test_cache = Cache(cache_path="/Users/rohitvernekar/Desktop/cache.pkl")
 
@@ -11,3 +12,9 @@ for i in range(200):
 
 # for key, value in test_cache._data.items():
 #     print(key, value)
+
+# Example usage
+processor = TweetDataProcessor("databaseteam8.c1coikyuqtk8.us-east-1.rds.amazonaws.com", "admin", "admin123", "databaseteam8")
+file_path = "C:/Users/obero/Desktop/corona-out-2"  # set file path
+user_data, tweet_data = processor.data_processing(file_path)
+#processor.insert_in_sql(tweet_data, user_data)
