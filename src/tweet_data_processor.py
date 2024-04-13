6697016f83e5ef9abcbc219438eb250b6ca3bc09
Sofia Pignataro <sofia.pignataro@gmail.com>
2024-04-13 19:20:32 -0400

    Updating tweet_data_processor.py and twiiter dataset counts

--- src/tweet_data_processor.py
+++ src/tweet_data_processor.py
@@ -1,41 +1,17 @@
 import json
-import mysql.connector
-from mysql.connector import Error
-import pandas as pd
-from sqlalchemy import create_engine
+import connections
 from datetime import datetime
 
 class TweetDataProcessor:
-    def __init__(self, host_name, user_name, user_password, db_name, port=3306):
-        self.host_name = host_name
-        self.user_name = user_name
-        self.user_password = user_password
-        self.db_name = db_name
-        self.port = port
-        self.connection = self.connect_to_mysql()
+    def __init__(self):
+        self.my_sqlconnection = connections.get_mysql_conn()
 
-    def connect_to_mysql(self):
-        connection = None
-        try:
-            connection = mysql.connector.connect(
-                host=self.host_name,
-                user=self.user_name,
-                passwd=self.user_password,
-                database=self.db_name,
-                port=self.port
-            )
-            print("Connection to MySQL DB successful")
-        except Error as e:
-            print(f"The error '{e}' occurred")
-        return connection
-
-    def fetch_user(self, user_data):
+    def fetch_user(self, user_data, post_creation):
         user = {
-            "user_id": user_data["id"],
-            "name": user_data["name"],
-            "screen_name": user_data["screen_name"],
-            "location": user_data.get("location", ""),
-            "description": user_data.get("description", ""),
+            "id_str": user_data["id_str"],
+            "name": user_data["name"].replace("'", "\\'").replace('"', '\\"'),
+            "screen_name": user_data["screen_name"].replace("'", "\\'").replace('"', '\\"'),
+            "protected": user_data["protected"],
             "verified": user_data["verified"],
             "followers_count": user_data["followers_count"],
             "friends_count": user_data["friends_count"],
@@ -43,10 +19,79 @@ class TweetDataProcessor:
             "favourites_count": user_data["favourites_count"],
             "statuses_count": user_data["statuses_count"],
             "created_at": self.parse_datetime(user_data["created_at"]),
-            "geo_enabled": user_data["geo_enabled"],
-            "profile_picture": user_data["profile_image_url_https"]
+            "last_post_timestamp": self.parse_datetime(post_creation)
         }
         return user
+    
+    def create_user_tb_mysql(self):
+        sql_setup = """CREATE TABLE IF NOT EXISTS users (
+                        id_str VARCHAR(255) NOT NULL,
+                        name VARCHAR(255),
+                        screen_name VARCHAR(255),
+                        protected BOOLEAN,
+                        verified BOOLEAN,
+                        followers_count INT,
+                        friends_count INT,
+                        listed_count INT,
+                        favourites_count INT,
+                        statuses_count INT,
+                        created_at TIMESTAMP,
+                        last_post_timestamp TIMESTAMP,
+                        PRIMARY KEY (id_str),
+                        INDEX (name),
+                        INDEX (screen_name)
+                        );
+                    """
+        try:
+            self.my_sqlconnection.cursor().execute(sql_setup)
+            self.my_sqlconnection.commit()
+        except Exception as e:
+            print(f"The error '{e}' occurred while creating table userdata in MySQL DB.")
+        
+    
+    def insert_into_user_tb_mysql(self, user):
+        
+        # Create table userdata if it does not exist yet
+        self.create_user_tb_mysql()
+ 
+        # Insert or update userdata records using user object
+        sql_insertion = f"""
+        REPLACE INTO users 
+        (
+            id_str,
+            name,
+            screen_name,
+            protected,
+            verified,
+            followers_count,
+            friends_count,
+            listed_count,
+            favourites_count,
+            statuses_count,
+            created_at,
+            last_post_timestamp
+        ) VALUES
+        (
+            "{user['id_str']}",
+            "{user['name']}",
+            "{user['screen_name']}",
+            {user['protected']},
+            {user['verified']},
+            {user['followers_count']},
+            {user['friends_count']},
+            {user['listed_count']},
+            {user['favourites_count']},
+            {user['statuses_count']},
+            TIMESTAMP("{user['created_at']}"),
+            GREATEST(COALESCE(last_post_timestamp,'1000-01-01 00:00:00'), TIMESTAMP("{user['last_post_timestamp']}"))
+        );
+        """
+        try:
+            self.my_sqlconnection.cursor().execute(sql_insertion)
+            self.my_sqlconnection.commit()
+        except Exception as e:
+            print(f"The error '{e}' occurred while inserting a record into table userdata in MySQL DB.")
+
 
     def fetch_tweet(self, tweet_data):
         tweet = {
@@ -57,61 +102,72 @@ class TweetDataProcessor:
         return tweet
 
     def parse_datetime(self, timestamp_str):
-        return datetime.strptime(timestamp_str, '%a %b %d %H:%M:%S %z %Y')
+        return datetime.strftime(datetime.strptime(timestamp_str, 
+                                '%a %b %d %H:%M:%S %z %Y'),
+                                '%Y-%m-%d %H:%M:%S')
 
     def data_processing(self, file_path):
+
+
         user_data = []
         tweet_data = []
+        
         with open(file_path, 'r') as file:
+
             for line in file:
+
                 if line != '\n':
+
                     data = json.loads(line)
-                    user = self.fetch_user(data['user'])
-                    tweet = self.fetch_tweet(data)
                     
-                    user_data.append(user)  # Add the user of the tweet
+                    # Insert user record into MySQL
+                    user = self.fetch_user(data['user'], data['created_at'])
+                    self.insert_into_user_tb_mysql(user)
+
+                    #tweet = self.fetch_tweet(data)
                     
                     if 'retweeted_status' in data:
+                          
                         # Handling retweet
                         try:
-                            user_data_rt = self.fetch_user(data['retweeted_status']['user'])
-                            tweet_data_rt = self.fetch_tweet(data['retweeted_status'])
-                            tweet_data_rt["retweet_id"] = None  # Indicate this tweet is a retweet
-                            user_data.append(user_data_rt)  # Add the user of the retweet
-                            tweet_data.append(tweet_data_rt)  # Add the retweeted tweet
+                            # Insert original retweet user record into MySQL
+                            user = self.fetch_user(data['retweeted_status']['user'], data['retweeted_status']['created_at'])
+                            self.insert_into_user_tb_mysql(user)
+
+                            #tweet_data_rt = self.fetch_tweet(data['retweeted_status'])
+                            #tweet_data_rt["retweet_id"] = None  # Indicate this tweet is a retweet
+                            #tweet_data.append(tweet_data_rt)  # Add the retweeted tweet
+
                         except Exception as e:
                             print(f"Error processing retweet: {e}")
+
                     else:
-                        tweet["retweet_id"] = None
+                        #tweet["retweet_id"] = None
+                        pass
+
+                    if 'quoted_status' in data:
+                        
+                        # Handling quoted tweet
+
+                        try:
+
+                            # Insert original quote tweet user record into MySQL
+                            user = self.fetch_user(data['quoted_status']['user'], data['quoted_status']['created_at'])
+                            self.insert_into_user_tb_mysql(user)
+
+                            # Do something about quote tweets here
+                            ###############################
+
+                        except Exception as e:
+                            print(f"Error processing quoted tweet: {e}")
+
+                    else:
+                        #tweet["quoted_tweet_id"] = None
+                        pass
                     
-                    tweet_data.append(tweet)  # Always add the original tweet
+                    
 
         return user_data, tweet_data
 
-    def insert_in_sql(self, tweet_data, user_data):
-        try:
-            engine = create_engine(f'mysql+mysqlconnector://{self.user_name}:{self.user_password}@{self.host_name}/{self.db_name}')
-            
-            # Convert user_data to DataFrame and remove duplicates
-            df_user = pd.DataFrame(user_data).drop_duplicates(['user_id'])
-            
-            # Convert tweet_data to DataFrame, remove duplicates and drop 'retweet_id' column
-            df_tweet = pd.DataFrame(tweet_data).drop_duplicates(['tweet_id']).drop('retweet_id', axis=1)
-            
-            # Insert Users first to avoid foreign key constraint failure
-            if not df_user.empty:
-                df_user.to_sql('users', con=engine, if_exists='append', index=False)
-                print("Users inserted successfully.")
-            else:
-                print("No user data to insert.")
-            
-            # Then insert Tweets
-            if not df_tweet.empty:
-                df_tweet.to_sql('tweets', con=engine, if_exists='append', index=False)
-                print("Tweets inserted successfully.")
-            else:
-                print("No tweet data to insert.")
-                
-        except Exception as e:
-            print(f"An error occurred: {e}")
+
 
