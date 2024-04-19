c2ede49315cc3b26de8fb549b2dea242bcd9fdf5
Harshith Oberoi <oberoiharshith@gmail.com>
2024-04-19 03:15:28 -0400

    usage sample added to main.py

--- main.py
+++ main.py
@@ -17,21 +17,15 @@ mongo_db_name = 'Database_team_8'
 #initiate
 twitter_queries = TwitterQueries()
 
-'''search_name = 'NUFF'
-sort_metric = 'timestamp'  # Could be 'timestamp', 'retweet', 'favorite', 'engagement'
-sort_order = -1  # -1 for descending, 1 for ascending
-df = twitter_queries.create_aggregated_username(search_name, sort_metric, sort_order)
-print(f"Aggregated Tweet Data for '{search_name}':")
-print(df)'''
-
-'''# Fetch and display user data sorted by followers count
-df_users_by_followers_desc = twitter_queries.search_and_sort_users('bob', sort_by='followers_count', order='desc')
-print("Users sorted by followers count (descending):")
-print(df_users_by_followers_desc)'''
-
 user_name = "NUFF"
 user_info = twitter_queries.get_user_ids_by_username(user_name)
 
 # Search for tweets by the user from the last week
 tweets = twitter_queries.search_tweets_username(user_info, '1week')
 print(tweets)
+
+# Fetch and display user data sorted by followers count
+df_users_by_followers_desc = twitter_queries.search_and_sort_users('bob', sort_by='followers_count', order='desc')
+print("Users sorted by followers count (descending):")
+print(df_users_by_followers_desc)
+
